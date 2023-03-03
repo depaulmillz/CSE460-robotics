c18ad4fe600b5be70916415cef7d54c0ee6989e5
dePaul Miller <depaulmillz@users.noreply.github.com>
2023-03-03 14:05:29 -0500

    Part 3

--- optitrack_practice/part3_q3.py
+++ optitrack_practice/part3_q3.py
@@ -7,9 +7,24 @@ sys.path.insert(0, "..")
 
 import trajectory.piecewise as piecewise
 
-def rect():
-    _, x, y, _, _ = piecewise.spline_2d([5., -3.8, -3.8, 5., 5.], [3.5, 3.5, -2.5, -2.5, 3.5], [0.,0.,0.,0.,0.], [0.,0.,0.,0.,0.], [0., 1., 2., 3., 4.], 100)
-    return [np.array([i, j]) for i,j in zip(x,y)]
+def rect(t, max_t):
+    
+    a = np.array([5., 3.5])
+    b = np.array([-3.8, 3.5])
+    c = np.array([-3.8, -2.5])
+    d = np.array([5.0, -2.5])
+
+    if t % max_t < max_t / 4:
+        return (b - a) * (t % max_t) / (max_t / 4) + a 
+    elif t % max_t < max_t / 2:
+        return (c - b) * (((t % max_t) - max_t / 4.) / (max_t / 4)) + b
+    elif t % max_t < 3. * max_t / 4.:
+        return (d - c) * (((t % max_t) - max_t / 2.) / (max_t / 4)) + c
+    else:
+        return (a - d) * (((t % max_t) - 3. * max_t / 4.) / (max_t / 4)) + d
+
+    #_, x, y, _, _ = piecewise.spline_2d([5., -3.8, -3.8, 5., 5.], [3.5, 3.5, -2.5, -2.5, 3.5], [0.,0.,0.,0.,0.], [0.,0.,0.,0.,0.], [0., 8.8, 13.8, 22.6, 27.6], 10)
+    #return [np.array([i, j]) for i,j in zip(x,y)]
 
 if __name__ == "__main__":
 
@@ -40,7 +55,7 @@ if __name__ == "__main__":
 
         x_t = np.array([xyz[0], xyz[0]])
 
-        positions = rect()#[np.array([5.,2.]), np.array([6.,2.]), np.array([6.,3.]), np.array([5.,3.])]
+        #positions = rect()#[np.array([5.,2.]), np.array([6.,2.]), np.array([6.,3.]), np.array([5.,3.])]
        
         #print(positions)
 
@@ -48,16 +63,18 @@ if __name__ == "__main__":
 
         pos = 0
 
-        x_d = np.copy(positions[pos])
+        #x_d = np.copy(positions[pos])
 
-        err = x_d - x_t
-        desiredAngle = np.arctan2(err[1], err[0])
+        #err = x_d - x_t
+        #desiredAngle = np.arctan2(err[1], err[0])
 
         print("robotx", "roboty", "desiredx", "desiredy", sep=",")
 
         while True:
             xyz, rot = position.get()
             x, y, _ = xyz
+
+            x_d = rect(time.time() - start, 100)
 
             x_t = np.array([x, y])
 
@@ -70,14 +87,13 @@ if __name__ == "__main__":
             angle = rot / 180 * np.pi
             desiredAngle = np.arctan2(err[1], err[0])
 
-            while dist <= 0.3:
-                pos += 1
-                pos = pos % (len(positions) - 1)
-                x_d = np.copy(positions[pos])
-                err = x_d - x_t
-                dist = np.linalg.norm(err)
-                desiredAngle = np.arctan2(err[1], err[0])
-
+            #while dist <= 0.4:
+            #    pos += 1
+            #    pos = pos % (len(positions) - 1)
+            #    x_d = np.copy(positions[pos])
+            #    err = x_d - x_t
+            #    dist = np.linalg.norm(err)
+            #    desiredAngle = np.arctan2(err[1], err[0])
                 
             #print(x_d, x_t)
 
